84d157066731a85b86307ed298ee191c07bc6dff
Krishna Yadav <90216712+krishna-yadav0@users.noreply.github.com>
2023-12-27 23:57:09 +0530

    Update UserController.java

--- src/main/java/com/registration/UserRegistration/controller/UserController.java
+++ src/main/java/com/registration/UserRegistration/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,10 +30,10 @@ public class UserController {
 
         return userService.getUser();
     }
-    
+
     @PostMapping("/save/{email}/{mobilenumber}/{username}")
     public String saveUser(@PathVariable String email, @PathVariable String mobilenumber,
-            @PathVariable String username){
+            @PathVariable String username) {
         return userService.saveUser(email, mobilenumber, username);
     }
 }
